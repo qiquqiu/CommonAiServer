f859ad9be0d4a2eefbcf037842e82daf825ea1d6
liyihan <2872458411@qq.com>
2025-06-30 12:54:46 +0800

    feat: Configure Spring MVC Async Request Executor

--- src/main/java/xyz/qiquqiu/aiserver/config/WebMvcConfig.java
+++ src/main/java/xyz/qiquqiu/aiserver/config/WebMvcConfig.java
@@ -3,12 +3,12 @@ package xyz.qiquqiu.aiserver.config;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.web.servlet.config.annotation.*;
 import xyz.qiquqiu.aiserver.interceptor.JwtTokenInterceptor;
 import xyz.qiquqiu.aiserver.properties.FileUploadProperties;
+
+import java.util.concurrent.Executor;
 
 /**
  * 配置类，注册web层相关组件
@@ -16,10 +16,11 @@ import xyz.qiquqiu.aiserver.properties.FileUploadProperties;
 @Slf4j
 @Configuration
 @RequiredArgsConstructor
-public class WebMvcConfiguration implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     private final JwtTokenInterceptor jwtTokenInterceptor;
     private final FileUploadProperties fileUploadProperties;
+    private final Executor customThreadPoolTaskExecutor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -44,5 +45,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
         //         .addResourceLocations("file:D:/dev/uploads/images/");
         registry.addResourceHandler(fileUploadProperties.getUrlPrefix() + "**")
                 .addResourceLocations("file:" + fileUploadProperties.getPath());
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setTaskExecutor((AsyncTaskExecutor) customThreadPoolTaskExecutor);
+        // 设置异步请求超时时间（毫秒）当一个异步请求在30秒内没有返回结果时，Spring MVC 会自动终止该请求并返回超时错误
+        configurer.setDefaultTimeout(30_000);
     }
 }
